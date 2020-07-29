35af3325824764884add5dc33dee5260534d6f7f
prashant sharma <prashant.sharma@enquero.com>
2020-07-29 22:06:48 +0530

    updated data file

--- src/data.js
+++ src/data.js
@@ -1,46 +1,6 @@
 export const data = [
   {
-    error: `Message               : Duplicate entry '6dad0a2b-173b-43e8-b41c-eb314456c927' for key 'PRIMARY'.
-    Element               : postInspectionFlow/processors/3 @ dota-qmp-sys-api-dev:post-inspection.xml:19 (QMP-T-Inspection)
-    Element XML           : <db:insert doc:name="QMP-T-Inspection" doc:id="1303d030-9a1a-4045-aa27-a630e09b6d6a" config-ref="MySQL_Database_Config_QMP">
-                            <db:sql>{file::sql/insert-qmp-inspection-result.sql}</db:sql>
-                            <db:input-parameters>#[vars.varQmpInspectionResultParams]</db:input-parameters>
-                            </db:insert>
-    Error type            : DB:QUERY_EXECUTION
-    Payload Type          : org.mule.runtime.core.internal.streaming.bytes.ManagedCursorStreamProvider`,
-    resolution: '42 is the answer to life',
-  },
-  {
-    error: `Message               : Duplicate entry '342d18cf-a276-446d-a8df-10e1ee0dce4b' for key 'PRIMARY'.
-    Element               : postInspectionFlow/processors/3 @ dota-qmp-sys-api-dev:post-inspection.xml:19 (QMP-T-Inspection)
-    Element XML           : <db:insert doc:name="QMP-T-Inspection" doc:id="1303d030-9a1a-4045-aa27-a630e09b6d6a" config-ref="MySQL_Database_Config_QMP">
-                            <db:sql>{file::sql/insert-qmp-inspection-result.sql}</db:sql>
-                            <db:input-parameters>#[vars.varQmpInspectionResultParams]</db:input-parameters>
-                            </db:insert>
-    Error type            : DB:QUERY_EXECUTION
-    Payload Type          : org.mule.runtime.core.internal.streaming.bytes.ManagedCursorStreamProvider`,
-    resolution: 'dont worry, Everything is ok',
-  },
-  {
-    error: `Message               : Duplicate entry '6dad0a2b-173b-43e8-b41c-eb314456c927' for key 'PRIMARY'.
-    Element               : postInspectionFlow/processors/3 @ dota-qmp-sys-api-dev:post-inspection.xml:19 (QMP-T-Inspection)
-    Element XML           : <db:insert doc:name="QMP-T-Inspection" doc:id="1303d030-9a1a-4045-aa27-a630e09b6d6a" config-ref="MySQL_Database_Config_QMP">
-                            <db:sql>{file::sql/insert-qmp-inspection-result.sql}</db:sql>
-                            <db:input-parameters>#[vars.varQmpInspectionResultParams]</db:input-parameters>
-                            </db:insert>
-    Error type            : DB:QUERY_EXECUTION
-    Payload Type          : org.mule.runtime.core.internal.streaming.bytes.ManagedCursorStreamProvider`,
-    resolution: 'Change 3 to 4',
-  },
-  {
-    error: `Message               : Duplicate entry '6dad0a2b-173b-43e8-b41c-eb314456c927' for key 'PRIMARY'.
-    Element               : postInspectionFlow/processors/3 @ dota-qmp-sys-api-dev:post-inspection.xml:19 (QMP-T-Inspection)
-    Element XML           : <db:insert doc:name="QMP-T-Inspection" doc:id="1303d030-9a1a-4045-aa27-a630e09b6d6a" config-ref="MySQL_Database_Config_QMP">
-                            <db:sql>{file::sql/insert-qmp-inspection-result.sql}</db:sql>
-                            <db:input-parameters>#[vars.varQmpInspectionResultParams]</db:input-parameters>
-                            </db:insert>
-    Error type            : DB:QUERY_EXECUTION
-    Payload Type          : org.mule.runtime.core.internal.streaming.bytes.ManagedCursorStreamProvider`,
-    resolution: 'This error is just to scare you',
+    error: `Put your error Here`,
+    resolution: 'Put your silution here',
   },
 ]
