26f16cee82852f10d8436acb3d012aaea5432ecb
prashant sharma <prashant.sharma@enquero.com>
2020-07-29 15:40:48 +0530

    Updates

---
+++ precache-manifest.1ac27e1b89a92deeabb0bedece4d08c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f57b6d607573a23fd4e191afa00d5b",
+    "url": "/text-compare-jaro-winkler/index.html"
+  },
+  {
+    "revision": "31c30253fd57c1b9ccd7",
+    "url": "/text-compare-jaro-winkler/static/css/main.dbd089cf.chunk.css"
+  },
+  {
+    "revision": "6eedc16cc3b6a2e3320a",
+    "url": "/text-compare-jaro-winkler/static/js/2.e37861c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/text-compare-jaro-winkler/static/js/2.e37861c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c30253fd57c1b9ccd7",
+    "url": "/text-compare-jaro-winkler/static/js/main.61584be9.chunk.js"
+  },
+  {
+    "revision": "1265a04882a732260feb",
+    "url": "/text-compare-jaro-winkler/static/js/runtime-main.98eeb4c4.js"
+  }
+]);
